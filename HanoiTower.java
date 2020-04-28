11adfb77ff1d0b345355e18dada1674705b34a14
Hussien-Talal-Majeed <hossientalal2@gmail.com>
2020-04-29 00:58:24 +0300

    added the function of the tower and calling it

--- HanoiTower.java
+++ HanoiTower.java
@@ -1,6 +1,20 @@
 public class HanoiTower{
+    static void tower(int discs, char from, char temp, char to){
+        if(discs == 1){
+            System.out.println("move disc 1 from "+from+" to "+to);
+            return;
+        }
+        else {
+            tower(discs-1,from,to,temp);
+            System.out.println("move disc "+ discs +" from "+from+" to "+to);
+            tower(discs-1,temp,from,to);
+        }
+    }
     public static void main(String[] args) {
-        
+        System.out.println("----------------Hanoi tower----------------");
+        int numberOfDiscs=3;
+        tower(numberOfDiscs, 'a', 'b', 'c');
 
     }
+    
 }
